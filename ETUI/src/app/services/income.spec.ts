0033a5e8f6c88b0543b285e8f6d96edd717252e0
Saksham Ojha <sakshamojha06@gmail.com>
2026-09-16 21:10:16 +0530

    feat: add Category, Income, and Expense services with corresponding unit tests

---
+++ ETUI/src/app/services/income.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { Income } from './income';
+
+describe('Income', () => {
+  let service: Income;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(Income);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
